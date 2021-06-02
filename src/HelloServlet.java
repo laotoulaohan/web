b971a2133113cf08cc5dc4d3da61ed8ec0a2ff9a
SYQ <2090230619@qq.com>
2021-06-02 11:25:29 +0800

    1

---
+++ src/HelloServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID=1L;
+    public void init(ServletConfig config)throws ServletException {
+        super.init(config);
+    }
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out=response.getWriter();
+        out.println("<html><body>");
+        out.println("第一个servlet类");
+        out.println("</body></html>");
+    }
+}
